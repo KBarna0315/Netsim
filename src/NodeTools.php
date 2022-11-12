0c276afbe16ae4ff3fb887a75864005e69988562
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-12 18:23:57 +0100

    Nodetools sintance and global node array created

--- src/NodeTools.php
+++ src/NodeTools.php
@@ -2,5 +2,18 @@
 
 class NodeTools
 {
+//Globals
+public $node = array(); //params of the node
 
+    /**
+     * Call this method to get singleton of NodeToolsClass.
+     * @return NodeTools
+     */
+    public static function Instance() {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new NodeTools();
+        }
+        return $inst;
+    }
 }
