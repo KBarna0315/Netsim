6a240eb97a8c8f1e2eed0c3f5f346ea0e95369dc
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-12 18:23:35 +0100

    Instance,create,delete development started

--- src/Device.php
+++ src/Device.php
@@ -1,6 +1,41 @@
 <?php
+/**
+ * Device Class
+ *
+ * Description: The Device related functions will be placed here
+ *
+ * @author Kiskovács Barna
+ */
 
-class Device
+
+class Device extends NodeTools
 {
-//Itt lesznek: PC,TV, Telefon, Router, Switch
+//Globals
+    public $device = array();
+
+    /**
+     * Call this method to get singleton of DeviceClass.
+     * @return Device
+     */
+    public static function Instance()
+    {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new Device();
+        }
+        return $inst;
+    }
+
+    //Contructor
+
+
+    public function createDevice()
+    {
+
+    }
+
+    public function deleteDevice($name){
+
+    }
+
 }
