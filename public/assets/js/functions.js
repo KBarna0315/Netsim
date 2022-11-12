afc6926a6fe229567649dd887c69aa9b7759cde6
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-12 18:10:29 +0100

    cross browser mouse position fix

---
+++ public/assets/js/functions.js
@@ -0,0 +1,33 @@
+/**
+ * Cross-Browser Mouse Event Handling.
+ * @param Event event is the Javascript event to handle in different browsers.
+ */
+function fixupMouse(event) {
+    let innerFrame = document.getElementById("nui_inner");
+    let innerFrameRect = innerFrame.getBoundingClientRect();
+    if(event.touches && event.touches[0]){
+        return {
+            event: event,
+            target: event.target,
+            which: event.which ? event.which : event.button === 1 ? 1 : event.button === 2 ? 3 : event.button === 4 ? 2 : 1,
+            x: event.touches[0].pageX - innerFrameRect.x + innerFrame.scrollLeft,
+            y: event.touches[0].pageY - innerFrameRect.y + innerFrame.scrollTop,
+            ctrlKey: event.ctrlKey,
+            altKey: event.altKey,
+            shiftKey: event.shiftKey,
+            buttons: event.buttons
+        };
+    } else {
+        return {
+            event: event,
+            target: event.target,
+            which: event.which ? event.which : event.button === 1 ? 1 : event.button === 2 ? 3 : event.button === 4 ? 2 : 1,
+            x: event.x - innerFrameRect.x + innerFrame.scrollLeft,
+            y: event.y - innerFrameRect.y + innerFrame.scrollTop,
+            ctrlKey: event.ctrlKey,
+            altKey: event.altKey,
+            shiftKey: event.shiftKey,
+            buttons: event.buttons
+        };
+    }
+}
