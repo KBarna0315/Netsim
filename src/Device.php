ef74ff2f3715315a9e3d1aaa92b73c97833f1d10
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-13 23:31:04 +0100

    Device creation started...

--- src/Device.php
+++ src/Device.php
@@ -28,9 +28,26 @@ class Device extends NodeTools
 
     //Contructor
 
-
-    public function createDevice()
+    /**
+     * Create device if not exists
+     * Called by Add New Node function
+     * @param string $diag the current diagram
+     * @param int $frameWidth is the Diagram Pane width in pixels.
+     * @param int $frameHeight is the Diagram Pane height in pixels.
+     * @param int $posLeft the left position of the new Device object according to the mouse click position
+     * @param int $posTop the top position of the new Device object according to the mouse click position
+     * @param array $object node assoc. array
+     */
+    public function createDevice(string $diag, int $frameWidth = 0, int $frameHeight = 0, int $posLeft = 0, int $posTop = 0,array $object = null)
     {
+        $this->diag = $diag;
+        $this->diagWidth = $frameWidth;
+        $this->diagHeight = $frameHeight;
+        $this->posLeft = $posLeft;
+        $this->posTop = $posTop;
+        if ($object) {
+            $this->node = $object;
+        }
 
     }
 
