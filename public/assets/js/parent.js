ec5ddef120fd00f917dabf02a81dd4811a3991a0
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-22 00:12:23 +0100

    other global container stuff

--- public/assets/js/parent.js
+++ public/assets/js/parent.js
@@ -1,1 +1,11 @@
 let globalContainer = {};
+globalContainer.isLoggedOut = false;
+globalContainer.ajaxUrlPrefix = '../src/ajax';
+globalContainer.jQueryToolsUrl = globalContainer.ajaxUrlPrefix + '/jQueryTools.php';
+
+$.ajaxSetup({
+    url: globalContainer.jQueryToolsUrl,
+    method: 'POST',
+    dataType: 'json',
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+});
