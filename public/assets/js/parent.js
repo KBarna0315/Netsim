f06844e5bc8a99762c41245d95311953f72d6031
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-22 01:12:58 +0100

    inspectorSelectValues added to get what will be displayed in the inspector field

--- public/assets/js/parent.js
+++ public/assets/js/parent.js
@@ -3,6 +3,12 @@ globalContainer.isLoggedOut = false;
 globalContainer.ajaxUrlPrefix = '../src/ajax';
 globalContainer.jQueryToolsUrl = globalContainer.ajaxUrlPrefix + '/jQueryTools.php';
 
+/**
+ * Used to store inspector select values, so we can filter them
+ * @type {{}}
+ */
+globalContainer.inspectorSelectValues = {};
+
 $.ajaxSetup({
     url: globalContainer.jQueryToolsUrl,
     method: 'POST',
