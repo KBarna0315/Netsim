09e6de7935f4f3221a79dd58a4e7bb865e1dffa4
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-25 02:27:31 +0100

    network db created

--- dbs/default.sql
+++ dbs/default.sql
@@ -20,3 +20,18 @@ CREATE TABLE `device`
     PRIMARY KEY (`Device_ID`),
     UNIQUE KEY `Name` (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `network`
+(
+    `Network_ID` int(11) NOT NULL AUTO_INCREMENT,
+    `Name`       varchar(255) NOT NULL,
+    `Network`    varchar(50) DEFAULT NULL,
+    `Netmask`    varchar(50) DEFAULT NULL,
+    `Gateway`    varchar(50) DEFAULT NULL,
+    `Type`       varchar(50) DEFAULT NULL,
+    `Left`       int(11) DEFAULT NULL,
+    `Right`      int(11) DEFAULT NULL,
+    `Top`        int(11) DEFAULT NULL,
+    `Bottom`     int(11) DEFAULT NULL,
+    PRIMARY KEY (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
