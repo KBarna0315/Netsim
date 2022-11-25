37a7674cf384b1e8c620100b8373f10d92638582
Kiskovacs Barna <kskovacsb@gmail.com>
2022-11-25 03:37:36 +0100

    checkObject() to check if there is an object with existing Name. not entirely sure im even going to use this

--- src/CommonTools.php
+++ src/CommonTools.php
@@ -78,5 +78,41 @@ class CommonTools
         return $objects;
     }
 
+    /**
+     * Check if the object exist by Name
+     * @param array $obj are the params of object
+     * + 'Name' is the name of the object, eg. Name field
+     * + 'ObjType' is the type of the object
+     * @param string type is the type of the object (optional), if not set, then $this->objType OR $node['objType'] give the type
+     * @public array $node are the params of the node
+     * + 'Name' is the name of the node to check
+     * @return number the number of row found, the result of $db->count
+     */
+    public function checkObject($obj = null, $objType = null) {
+        $db = DB::Instance();
+        $query = '';
+        $queryParams = [];
+        if (is_string($obj) && $objType) {
+            $table = strtolower($objType);
+            if ($table) {
+                $query = "SELECT * FROM `$table` WHERE `Name`=:Name";
+                $queryParams = ['Name' => $obj];
+            }
+        } elseif (is_array($obj)) {
+            if ($objType) {
+                $table = strtolower($objType);
+            } else if ($obj['objType']) {
+                $table = strtolower($obj['objType']);
+            }
+            if ($table && $obj['Name']) {
+                $query = "SELECT * FROM `$table` WHERE `Name`=:Name";
+                $queryParams = ['Name' => $obj['Name']];
+            }
+        }
+        if ($query && $queryParams) {
+            return $db->count(SessionManager::getDB(), $query, $queryParams);
+        }
+    }
+
 }
 
